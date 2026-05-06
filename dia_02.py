bc98e82ba3cbf761a282832eed2407ddc8d6f76b
Adrielle Cristian <dricalove1@hotmail.com>
2026-05-06 17:29:26 -0300

    Dia-02

---
+++ dia_02.py
@@ -0,0 +1,22 @@
+
+numero1 = int(input("Digite o primeiro numero: "))
+
+numero2 = int(input("Digite o segundo numero: "))
+
+soma = numero1 + numero2
+
+subtracao = numero1 - numero2
+
+divisao = numero1 / numero2
+
+multiplicacao = numero1 * numero2
+
+print(f"\n********RESULTADOS**********")
+
+print(f"Soma: {soma}")
+
+print(f"Subtração: {subtracao}")
+
+print(f"Divisão: {divisao}")
+
+print(f"Multiplicação: {multiplicacao}")
