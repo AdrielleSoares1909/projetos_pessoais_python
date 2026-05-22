f02aeb1067ebf8933f3ab7705a6eca35f13bd93e
Adrielle Cristian <dricalove1@hotmail.com>
2026-05-22 16:27:31 -0300

    Dia-08

---
+++ dia_08.py
@@ -0,0 +1,40 @@
+"""
+
+📦 LISTAS
+
+Crie uma lista chamada:
+
+nomes
+
+Depois:
+
+✅ peça 3 nomes
+✅ guarde na lista
+✅ mostre a lista final
+
+O for quer dizer:
+
+"para cada item"
+
+📦 for
+
+Percorre itens.
+
+"""
+
+nome1 = input("Digite o primeiro nome: ")
+nome2 = input("Digite o segundo nome: ")
+nome3 = input("Digite o terceiro nome: ")
+
+nomes =[nome1,nome2,nome3]
+
+#para cada nome dentro da lista...
+# variável temporária (nome)
+
+for nome in nomes: #para cada nome na lista nomes print o nome
+    print(nome)
+
+
+
+
+
